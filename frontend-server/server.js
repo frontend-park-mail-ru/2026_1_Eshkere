4ba3d500a6af3d1dd4fda978d08d45e71da0b690
current3 <urazgaliev.dmitrii02102005@gmail.com>
2026-03-09 22:10:45 +0300

    server

---
+++ frontend-server/server.js
@@ -0,0 +1,17 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const publicDir = path.join(__dirname, '..', 'frontend');
+
+app.use(express.static(publicDir));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(publicDir, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Frontend server is running on http://localhost:${PORT}`);
+});
