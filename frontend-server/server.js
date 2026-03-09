fa7d35cc542f2a2e6adbb9acc24d3231157fa204
current3 <urazgaliev.dmitrii02102005@gmail.com>
2026-03-10 02:47:35 +0300

    Server path

--- frontend-server/server.js
+++ frontend-server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicDir = path.join(__dirname, '..', 'frontend');
+const publicDir = path.join(__dirname, '..');
 
 app.use(express.static(publicDir));
 
